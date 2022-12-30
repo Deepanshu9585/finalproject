cbf628e748a768d4e7468e5fc7e08d24d609b6f1
Deepanshu9585 <107471930+Deepanshu9585@users.noreply.github.com>
2022-12-30 14:26:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function submitForm() {
+    let fname = document.getElementById('fname').value;
+    let lname = document.getElementById('lname').value;
+    let email = document.getElementById('email').value;
+    let reason = document.getElementById('reason').value;
+
+    if (fname != null && lname != null && email != null && reason != null){
+        alert("Form sumitted successfully")
+    }
+    else{
+        alert("All fields required")
+    }
+}
